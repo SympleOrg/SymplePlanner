08b1b4ff432683993c0c1fe26cf574059d421451
ImBonana <eladtairo@gmail.com>
2025-06-04 20:47:13 +0300

    Add @get:JvmName to shouldRunActionWhileMoving for better naming

--- SymplePlanner/src/main/java/top/symple/sympleplanner/trajectory/TrajectoryState.kt
+++ SymplePlanner/src/main/java/top/symple/sympleplanner/trajectory/TrajectoryState.kt
@@ -3,7 +3,7 @@ package top.symple.sympleplanner.trajectory
 import top.symple.sympleplanner.geometry.Pose2d
 import top.symple.sympleplanner.interfaces.IAction
 
-data class TrajectoryState(val pose2d: Pose2d?, val action: IAction?, val shouldRunActionWhileMoving: Boolean, val desiredRobotVelocityMPS: Double?) {
+data class TrajectoryState(val pose2d: Pose2d?, val action: IAction?, @get:JvmName("shouldRunActionWhileMoving") val shouldRunActionWhileMoving: Boolean, val desiredRobotVelocityMPS: Double?) {
     constructor(action: IAction, shouldRunActionWhileMoving: Boolean) : this(null, action, shouldRunActionWhileMoving, null)
     constructor(pose2d: Pose2d) : this(pose2d, null, true, null)
     constructor(pose2d: Pose2d, desiredRobotVelocityMPS: Double) : this(pose2d, null, true, desiredRobotVelocityMPS)
